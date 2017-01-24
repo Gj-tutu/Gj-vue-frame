b40ee95b7488e99bb6a8ebc4c239d3285761c9da
tutu <391476634@qq.com>
2017-01-24 17:11:51 +0800

    first commit

---
+++ build/webpack.config.js
@@ -0,0 +1,202 @@
+const webpack = require('webpack')
+const cssnano = require('cssnano')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const config = require('../config')
+const debug = require('debug')('app:webpack:config')
+
+const paths = config.utils_paths
+const __DEV__ = config.globals.__DEV__
+const __PROD__ = config.globals.__PROD__
+const __TEST__ = config.globals.__TEST__
+
+debug('Creating configuration.')
+const webpackConfig = {
+  name: 'client',
+  target: 'web',
+  devtool: config.compiler_devtool,
+  resolve: {
+    root: paths.client(),
+    extensions: ['', '.js', '.vue', '.json']
+  },
+  module: {}
+}
+// ------------------------------------
+// Entry Points
+// ------------------------------------
+const APP_ENTRY = paths.client('main.js')
+
+webpackConfig.entry = {
+  app: __DEV__
+    ? [APP_ENTRY].concat(`webpack-hot-middleware/client?path=${config.compiler_public_path}__webpack_hmr`)
+    : [APP_ENTRY],
+  vendor: config.compiler_vendors
+}
+
+// ------------------------------------
+// Bundle Output
+// ------------------------------------
+webpackConfig.output = {
+  filename: `js/[name].[${config.compiler_hash_type}].js`,
+  path: paths.dist(),
+  publicPath: config.compiler_public_path
+}
+
+// ------------------------------------
+// Plugins
+// ------------------------------------
+webpackConfig.plugins = [
+  new webpack.DefinePlugin(config.globals),
+  new HtmlWebpackPlugin({
+    template: paths.client('index.html'),
+    hash: false,
+    // favicon: paths.client('static/favicon.ico'),
+    filename: 'index.html',
+    inject: 'body',
+    minify: {
+      collapseWhitespace: true
+    },
+    globals: config.globals
+  })
+]
+
+if (__DEV__) {
+  debug('Enable plugins for live development (HMR, NoErrors).')
+  webpackConfig.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  )
+} else if (__PROD__ || __TEST__) {
+  debug('Enable plugins for production (OccurenceOrder, Dedupe & UglifyJS).')
+  webpackConfig.plugins.push(
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module, count) {
+        // any required modules inside node_modules are extracted to vendor
+        return (
+          module.resource &&
+          /\.js$/.test(module.resource) &&
+          module.resource.indexOf(
+            path.join(__dirname, '../node_modules')
+          ) === 0
+        )
+      }
+    }),
+    // extract webpack runtime and module manifest to its own file in order to
+    // prevent vendor hash from being updated whenever app bundle is updated
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest',
+      chunks: ['vendor']
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  )
+}
+
+// ------------------------------------
+// Loaders
+// ------------------------------------
+// File loaders
+// JavaScript / JSON
+webpackConfig.module.loaders = [
+  {
+    test: /\.vue$/,
+    loader: 'vue'
+  },
+  {
+    test: /\.js$/,
+    loader: 'babel',
+    exclude: /node_modules/
+  },
+  {
+    test: /\.json$/,
+    loader: 'json'
+  },
+  {
+    test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+    loader: 'url',
+    query: {
+      limit: 10000,
+      name: 'img/[name].[hash:7].[ext]'
+    }
+  },
+  {
+    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+    loader: 'url',
+    query: {
+      limit: 10000,
+      name: 'fonts/[name].[hash:7].[ext]'
+    }
+  }
+]
+
+webpackConfig.module.loaders.push({
+  test: /\.scss$/,
+  exclude: null,
+  loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader!postcss-loader')
+})
+
+webpackConfig.module.loaders.push({
+  test: /\.css$/,
+  exclude: null,
+  loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader')
+})
+
+webpackConfig.sassLoader = {
+  includePaths: paths.client('styles')
+}
+
+webpackConfig.postcss = [
+  cssnano({
+    autoprefixer: {
+      add: true,
+      remove: true,
+      browsers: ['last 2 versions']
+    },
+    discardComments: {
+      removeAll: true
+    },
+    discardUnused: false,
+    mergeIdents: false,
+    reduceIdents: false,
+    safe: true,
+    sourcemap: true
+  })
+]
+// ------------------------------------
+// Style Loaders
+// ------------------------------------
+// We use cssnano with the postcss loader, so we tell
+// css-loader not to duplicate minimization.
+
+webpackConfig.vue = {
+  loaders: {
+    'css': ExtractTextPlugin.extract('vue-style-loader', 'css-loader!postcss-loader'),
+    'scss': ExtractTextPlugin.extract('vue-style-loader', 'css-loader!sass-loader!postcss-loader')
+  }
+}
+
+/* eslint-disable */
+
+/* eslint-enable */
+
+// ------------------------------------
+// Finalize Configuration
+// ------------------------------------
+// when we don't know the public path (we know it only when HMR is enabled [in development]) we
+// need to use the extractTextPlugin to fix this issue:
+// http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809
+
+webpackConfig.plugins.push(
+  new ExtractTextPlugin('css/[name].[contenthash].css', {
+    allChunks: true
+  })
+)
+
+module.exports = webpackConfig
